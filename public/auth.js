bd0d069dbc9538312cbe098f5aebcf5693643814
yashkumargupta12345 <guptayashyg45227@gmail.com>
2025-02-04 00:13:38 +0530

    After deployment URL removal

--- public/auth.js
+++ public/auth.js
@@ -8,7 +8,7 @@ async function checkAuth() {
     }
     
     try {
-        const response = await fetch('https://daily-task-management-zmdu.onrender.com/api/users/' + currentUser.id + '/todos', {
+        const response = await fetch('/api/users/' + currentUser.id + '/todos', {
             headers: {
                 'Authorization': 'Bearer ' + token
             }
@@ -29,7 +29,7 @@ async function checkAuth() {
     }
 }
 
-const API_URL = 'https://daily-task-management-zmdu.onrender.com';
+const API_URL = '';
 
 // Function to handle login
 async function login(event) {
